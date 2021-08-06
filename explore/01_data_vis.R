6e6e43264867bc9f534ddc3a46237fe831249931
karlvurdst <karl.tjensvoll@vurdst.dk>
2021-08-06 11:07:55 +0000

    First commit

---
+++ explore/01_data_vis.R
@@ -0,0 +1,14 @@
+# How to check for current directory
+getwd()
+
+# Get OS info
+Sys.info()
+
+# Some git suff
+#library(usethis)
+# usethis::create_github_token()  # Create token
+#library(credentials)
+#credentials::set_github_pat()  # Set token
+
+### Here starts excercise
+
